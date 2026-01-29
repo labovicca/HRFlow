4ae0ed871717bb3a5d4606e3fdb30f9e29d2e4f2
Nikola Milosevic <nikola9995@yahoo.com>
2026-01-29 22:42:14 +0100

    add swagger

--- HRFlow/services/AuthenticationService/Program.cs
+++ HRFlow/services/AuthenticationService/Program.cs
@@ -2,14 +2,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
-
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    
+    app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Authentication API V1");
+        }
+    );
 }
 
 app.UseHttpsRedirection();
