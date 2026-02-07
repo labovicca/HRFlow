9507b420bdca5b5187093276f72c1e8bb6344616
Aleksandra Labovic <labovicaleksandra@gmail.com>
2026-02-07 15:59:13 +0100

    Set up Program.cs

---
+++ HRFlow/Services/EmployeeService/EmployeeService.API/Program.cs
@@ -0,0 +1,25 @@
+using EmployeeService.Common.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services.AddEmployeeServices();
+builder.Services.AddDocumentServices();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
